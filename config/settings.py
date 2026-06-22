5014c21e5812ceae029a5d891824031dc9daf1cd
pradrao0 <pradh.rao0@gmail.com>
2026-06-22 20:33:30 +0530

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")    
+ALLOWED_HOSTS = ["*"]    
 
 
 # -----------------------------
