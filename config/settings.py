6f77c6329e0cec67b6b39e9010724f1af8cde38d
pradrao0 <pradh.rao0@gmail.com>
2026-06-22 20:19:43 +0530

    UpdateD settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,11 +9,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-temporary-key-for-debug"
+))
 
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")    
 
 
 # -----------------------------
